db7c056a4b2477c2c9fbefaa2ddd56e4ee0fe9ed
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-11-05 00:34:52 +0700

    feat : analytics

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,8 @@ import { logout } from "../controllers/auth/logout";
 import { likeTo } from "../controllers/like/like";
 import { listenTo } from "../controllers/listen/listen";
 import { createPoster } from "../controllers/poster/poster";
+import { highestLikeByAlbum, highestLikeByMusic } from "../controllers/like/analytic";
+import { highestListenByAlbum, highestListenByMusic } from "../controllers/listen/analytic";
 
 router.post("/register", register)
 router.post("/login", login)
@@ -17,4 +19,9 @@ router.post("/listen",listenTo)
 
 router.post("/poster",createPoster)
 
+router.get("/music/like",highestLikeByMusic)
+router.get("/music/listen",highestListenByMusic)
+
+router.get("/album/like",highestLikeByAlbum)
+router.get("/album/listen",highestListenByAlbum)
 export default router;
