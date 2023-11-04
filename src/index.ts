921601dcdf21a7da65f2fe1d334e8fe80d7f3356
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-11-04 18:31:11 +0700

    feat : router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import * as dotenv from 'dotenv';
 import express  from 'express';
 import cors from 'cors';
 import router from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 if (!process.env.PORT) {
@@ -14,8 +17,10 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
 app.use(express.json()); 
+app.use(cookieParser());
 app.use(cors());
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log(`Listening on  http://localhost:${PORT}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,18 @@ const router = express.Router();
 
 import { register } from "../controllers/auth/register";
 import { login } from "../controllers/auth/login";
+import { logout } from "../controllers/auth/logout";
+import { likeTo } from "../controllers/like/like";
+import { listenTo } from "../controllers/listen/listen";
+import { createPoster } from "../controllers/poster/poster";
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", register)
+router.post("/login", login)
+router.get("/logout",logout)
+
+router.post("/like",likeTo)
+router.post("/listen",listenTo)
+
+router.post("/poster",createPoster)
 
 export default router;
